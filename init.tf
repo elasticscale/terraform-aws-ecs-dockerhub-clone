02b49e9f06d318a585c1aa184c453752ec200300
Alex <alex@vontis.nl>
2023-07-31 10:29:43 +0200

    describe output

--- init.tf
+++ init.tf
@@ -1,8 +1,9 @@
 
 resource "null_resource" "init" {
   triggers = {
-    containers = local.repolist
-    name       = aws_codebuild_project.main.name
+    containers     = local.repolist
+    name           = aws_codebuild_project.main.name
+    build_commands = local.buildstrings
   }
   provisioner "local-exec" {
     command = "aws codebuild start-build --project-name ${aws_codebuild_project.main.name} --region ${data.aws_region.current.name} > /dev/null"
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
 output "image_base_url" {
-  value = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com/${var.namespace}/"
+  description = "The base URL for your ECR images from Docker Hub"
+  value       = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com/${var.namespace}/"
 }
