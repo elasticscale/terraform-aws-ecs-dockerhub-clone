ef7314ccd95c84545b66068df31a38a388f2e1e0
Alex <alex@vontis.nl>
2023-07-31 10:31:21 +0200

    Update init.tf

--- init.tf
+++ init.tf
@@ -3,7 +3,7 @@ resource "null_resource" "init" {
   triggers = {
     containers     = local.repolist
     name           = aws_codebuild_project.main.name
-    build_commands = local.buildstrings
+    build_commands = jsonencode(local.buildstrings)
   }
   provisioner "local-exec" {
     command = "aws codebuild start-build --project-name ${aws_codebuild_project.main.name} --region ${data.aws_region.current.name} > /dev/null"
